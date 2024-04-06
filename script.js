576d945f0386e1ad98d421a069b18580c13fd205
Subrata Mondal <subratamondal1029@outlook.com>
2024-04-06 13:21:37 +0530

    All complete with payment

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//image slider
 const swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     mousewheel: true,
@@ -7,6 +8,7 @@ const swiper = new Swiper(".mySwiper", {
       },
   });
 
+  //testimonial swiper
 const swiper2 = new Swiper(".mySwiper2", {
     slidesPerView: 1.5,
     spaceBetween: 10,
@@ -17,3 +19,29 @@ const swiper2 = new Swiper(".mySwiper2", {
     loop: true,
     mousewheel: true,
   });
+
+
+//payment geatway
+
+const download = document.getElementById('DownloadPdf');
+download.addEventListener('click', onButtonClick);
+
+function onButtonClick() {
+  Instamojo.open('https://test.instamojo.com/@subratamondal/l1e1917a4d77e4afd82eb71270e5eb1ff/');
+}
+
+// Start client-defined Callback Handler Functions 
+function onPaymentSuccessHandler (response) {
+  setTimeout(() => {
+    window.open('https://drive.google.com/file/d/1uAVJpN5u3iX3iXnc_aYVR7vMgNo9_yBG/view?usp=drive_link');
+  }, 2000);
+}
+
+// End client-defined Callback Handler Functions 
+
+// Configuring Handlers 
+Instamojo.configure({
+  handlers: {
+    onSuccess: onPaymentSuccessHandler,
+  }
+});
